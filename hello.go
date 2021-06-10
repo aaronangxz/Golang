9c1e4379c4e95d5602df6473131a630e2e47cca3
aaronangxz <angxze@gmail.com>
2021-06-10 22:07:48 +0800

    Added fmt functions

--- hello.go
+++ hello.go
@@ -2,15 +2,78 @@ package main
 
 import "fmt"
 
+/* ------------------
+fmt package
+
+1. printf,%v (%v handles int,bool,string etc.)
+	name := "Elon"
+	fmt.Printf("Name = %v",name)
+
+	>> Name = Elon
+
+2. printf,%s %d
+	name := "Elon"
+	age := 69
+	fmt.Printf("Name is %s",name)
+	fmt.Printf("Age is %d",age)
+
+	>> Name is Elon
+	>> Age is 69
+
+3. println
+	fmt.Prinln("Hello", 123)
+
+	fmt.Println() //new line
+
+	items := []int{10,20,30}
+	fmt.Println(items)
+
+	println(items) (no 'fmt', prints ref value)
+
+	>> Hello 123
+	>> [10,20,30]
+	>> [3/3]0xc083927498a7684
+
+4. print
+	elements := []int{999,99,9}
+
+	for i := 0; i < len(elements); i++{
+		fmt.Print(elements[i] + 1)
+		fmt.Print(" ")
+	}
+
+	>> 1000 100 10
+
+5. sprintf (like printf but returns a string, not printed out)
+
+5. sprintln (like println but returns a string, not printed out)
+
+------------------ */
+
 func main() {
 	x, y, z := 1, 2, 3
 	fmt.Println(add(x, y))
 	fmt.Println(add(y, z))
 	fmt.Println("Hello World!")
+	forloop()
 }
 
 func add(x int, y int) int {
 	return x + y
+}
+
+func forloop() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		fmt.Printf("\n")
+	}
+}
+
+func loop() {
+	variables := []string{"A", "B", "C"}
+	for i, j := range variables {
+		fmt.Println(i, j)
+	}
 }
 
 //Run
